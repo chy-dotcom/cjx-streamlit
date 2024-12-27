98655380928f1522f4d1e7ffb87e0d3fd66ea4a0
chy-dotcom <3141115186@qq.com>
2024-12-27 15:03:36 +0800

    Update app.py

--- app.py
+++ app.py
@@ -7,11 +7,14 @@ from collections import Counter
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
 import io
+import matplotlib.font_manager as fm
 
 
 # 设置字体，确保支持中文
-matplotlib.rcParams['font.family'] = 'sans-serif'
-matplotlib.rcParams['font.sans-serif'] = ['SimHei']
+font_path = 'font/SimHei.ttf' 
+font_prop = fm.FontProperties(fname=font_path)
+plt.rcParams['font.family'] = font_prop.get_name()
+plt.rcParams['axes.unicode_minus'] = False
 
 # 加载停用词的函数
 def load_stopwords(file_path):
